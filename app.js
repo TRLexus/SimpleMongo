fdb47e61dc0e26651b9cae4b1efb326aa48c0c95
TRLexus <95573385+TRLexus@users.noreply.github.com>
2023-12-29 10:23:02 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,42 +1,120 @@
-document.addEventListener("DOMContentLoaded", function () {
-    loadBooks();
+$(document).ready(function() {
+    displayMahasiswa();
+
+    function displayMahasiswa() {
+        $.ajax({
+            url: '/mahasiswa',
+            type: 'GET',
+            success: function(data) {
+                $('#mahasiswaList').empty();
+                data.forEach(function(mahasiswa) {
+                    $('#mahasiswaList').append(`
+                        <li class="list-group-item" onclick="editMahasiswa('${mahasiswa._id}', '${mahasiswa.nama}', '${mahasiswa.nim}', '${mahasiswa.jurusan}', ${mahasiswa.semester}, ${mahasiswa.ipk})">
+                            <strong>${mahasiswa.nama}</strong> (${mahasiswa.nim}) - ${mahasiswa.jurusan}, Semester ${mahasiswa.semester}, IPK ${mahasiswa.ipk}
+                            <button class="btn btn-danger btn-sm float-right" onclick="deleteMahasiswa('${mahasiswa._id}')">Hapus</button>
+                        </li>
+                    `);
+                });
+            }
+        });
+    }
+
+    function showNotification(message, type) {
+        $('#notification').remove(); // Hapus pemberitahuan sebelumnya jika ada
+
+        $('body').append(`
+            <div id="notification" class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-delay="3000">
+                <div class="toast-header">
+                    <strong class="mr-auto">${type === 'success' ? 'Sukses' : 'Error'}</strong>
+                    <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="toast-body">${message}</div>
+            </div>
+        `);
+
+        $('.toast').toast('show');
+    }
+
+    window.addMahasiswa = function() {
+        var nama = $('#nama').val();
+        var nim = $('#nim').val();
+        var jurusan = $('#jurusan').val();
+        var semester = $('#semester').val();
+        var ipk = $('#ipk').val();
+
+        $.ajax({
+            url: '/mahasiswa',
+            type: 'POST',
+            data: {
+                nama: nama,
+                nim: nim,
+                jurusan: jurusan,
+                semester: semester,
+                ipk: ipk
+            },
+            success: function() {
+                displayMahasiswa();
+                showNotification('Mahasiswa ditambahkan!', 'success');
+                $('#nama').val('');
+                $('#nim').val('');
+                $('#jurusan').val('');
+                $('#semester').val('');
+                $('#ipk').val('');
+            },
+            error: function(err) {
+                showNotification('Gagal menambahkan mahasiswa. Periksa kembali inputan Anda.', 'error');
+            }
+        });
+    }
+
+    window.editMahasiswa = function(id, nama, nim, jurusan, semester, ipk) {
+        $('#nama').val(nama);
+        $('#nim').val(nim);
+        $('#jurusan').val(jurusan);
+        $('#semester').val(semester);
+        $('#ipk').val(ipk);
+
+        $('#mahasiswaForm').append('<button type="button" onclick="updateMahasiswa(\'' + id + '\')">Update</button>');
+    }
+
+    window.updateMahasiswa = function(id) {
+        var nama = $('#nama').val();
+        var nim = $('#nim').val();
+        var jurusan = $('#jurusan').val();
+        var semester = $('#semester').val();
+        var ipk = $('#ipk').val();
+
+        $.ajax({
+            url: '/mahasiswa/' + id,
+            type: 'PUT',
+            data: {
+                nama: nama,
+                nim: nim,
+                jurusan: jurusan,
+                semester: semester,
+                ipk: ipk
+            },
+            success: function() {
+                displayMahasiswa();
+                $('#nama').val('');
+                $('#nim').val('');
+                $('#jurusan').val('');
+                $('#semester').val('');
+                $('#ipk').val('');
+                $('button:contains("Update")').remove();
+            }
+        });
+    }
+
+    window.deleteMahasiswa = function(id) {
+        $.ajax({
+            url: '/mahasiswa/' + id,
+            type: 'DELETE',
+            success: function() {
+                displayMahasiswa();
+            }
+        });
+    }
 });
-
-function loadBooks() {
-    fetch("/api/books")
-        .then(response => response.json())
-        .then(data => {
-            displayBooks(data);
-        })
-        .catch(error => console.error("Error fetching books:", error));
-}
-
-function displayBooks(books) {
-    const bookList = document.getElementById("bookList");
-    bookList.innerHTML = "";
-
-    books.forEach(book => {
-        const listItem = document.createElement("li");
-        listItem.textContent = `${book.title} by ${book.author}`;
-        bookList.appendChild(listItem);
-    });
-}
-
-function addBook() {
-    const title = document.getElementById("title").value;
-    const author = document.getElementById("author").value;
-
-    fetch("/api/add", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ title, author })
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log("Book added:", data);
-            loadBooks(); // Refresh the book list after adding a book
-        })
-        .catch(error => console.error("Error adding book:", error));
-}
--- server.js
+++ server.js
@@ -1,54 +1,96 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const MongoClient = require("mongodb").MongoClient;
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
-app.use(express.static("public"));
 app.use(bodyParser.json());
 
-// MongoDB connection string (replace with your MongoDB connection details)
-const mongoURI = "mongodb://localhost:27017";
-const dbName = "booksdb";
+mongoose.connect('mongodb://localhost:27017/mahasiswaDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
-MongoClient.connect(mongoURI, { useUnifiedTopology: true }, (err, client) => {
-    if (err) {
-        console.error("Error connecting to MongoDB:", err);
-        return;
-    }
-
-    console.log("Connected to MongoDB");
-
-    const db = client.db(dbName);
-    const collection = db.collection("books");
-
-    // API to get all books
-    app.get("/api/books", (req, res) => {
-        collection.find().toArray((err, books) => {
-            if (err) {
-                console.error("Error fetching books:", err);
-                res.status(500).json({ error: "Internal Server Error" });
-                return;
-            }
-            res.json(books);
-        });
-    });
-
-    // API to add a book
-    app.post("/api/add", (req, res) => {
-        const { title, author } = req.body;
-        collection.insertOne({ title, author }, (err, result) => {
-            if (err) {
-                console.error("Error adding book:", err);
-                res.status(500).json({ error: "Internal Server Error" });
-                return;
-            }
-            res.json({ success: true });
-        });
-    });
+const mahasiswaSchema = new mongoose.Schema({
+    nama: String,
+    nim: String,
+    jurusan: String,
+    semester: Number,
+    ipk: Number
 });
 
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+const Mahasiswa = mongoose.model('Mahasiswa', mahasiswaSchema);
+
+// Data Dummy JSON
+const dummyData = [
+    {
+        nama: 'John Doe',
+        nim: '123456',
+        jurusan: 'Teknik Informatika',
+        semester: 3,
+        ipk: 3.5
+    },
+    {
+        nama: 'Jane Doe',
+        nim: '789012',
+        jurusan: 'Sistem Informasi',
+        semester: 2,
+        ipk: 3.2
+    },
+    {
+        nama: 'Bob Smith',
+        nim: '345678',
+        jurusan: 'Teknik Elektro',
+        semester: 4,
+        ipk: 3.8
+    }
+];
+
+// Tambahkan Data Dummy ke MongoDB
+Mahasiswa.insertMany(dummyData, function (err) {
+    if (err) {
+        console.error('Error initializing dummy data:', err);
+    } else {
+        console.log('Dummy data initialized successfully!');
+    }
+});
+
+// CRUD Routes
+app.get('/mahasiswa', async (req, res) => {
+    try {
+        const mahasiswa = await Mahasiswa.find();
+        res.json(mahasiswa);
+    } catch (err) {
+        res.json({ message: err.message });
+    }
+});
+
+app.post('/mahasiswa', async (req, res) => {
+    const mahasiswa = new Mahasiswa(req.body);
+    try {
+        await mahasiswa.save();
+        res.json('Mahasiswa added!');
+    } catch (err) {
+        res.json({ message: err.message });
+    }
+});
+
+app.put('/mahasiswa/:id', async (req, res) => {
+    try {
+        await Mahasiswa.findByIdAndUpdate(req.params.id, req.body);
+        res.json('Mahasiswa updated!');
+    } catch (err) {
+        res.json({ message: err.message });
+    }
+});
+
+app.delete('/mahasiswa/:id', async (req, res) => {
+    try {
+        await Mahasiswa.findByIdAndDelete(req.params.id);
+        res.json('Mahasiswa deleted!');
+    } catch (err) {
+        res.json({ message: err.message });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
